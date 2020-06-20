4e0f43236c6eb986d3537931eccae0acaae76159
PandeDarma <pandedarma99@gmail.com>
2020-06-20 19:36:54 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/latihan_qrbarcode/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.latihan_qrbarcode
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+
+import './view_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String kode = "";
+  var getKode;
+  Future scanBarcode() async {
+    getKode = await BarcodeScanner.scan();
+    setState(() {
+      kode = getKode.rawContent;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Belajar QR Code"),
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+        elevation: 0.0,
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 10,
+            decoration: BoxDecoration(
+              color: Colors.purpleAccent,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(16.0),
+                bottomRight: Radius.circular(16.0),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Image.network(
+              "https://www.seekpng.com/png/detail/26-265563_other-qr-code-ideas-scan-qr-code-png.png"),
+          SizedBox(
+            height: 10.0,
+          ),
+          FlatButton(
+            onPressed: () {
+              scanBarcode();
+            },
+            child: Container(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Center(
+                  child: Text(
+                    "Scan Here",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.purpleAccent,
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              ),
+            ),
+          ),
+          FlatButton(
+            onPressed: kode == ""
+                ? () {}
+                : () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => WebScreen(kode),
+                      ),
+                    );
+                  },
+            child: Container(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Center(
+                  child: kode == ""
+                      ? Text(
+                          "Result",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 18,
+                          ),
+                        )
+                      : Text(
+                          kode,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                            fontSize: 16,
+                          ),
+                        ),
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.purpleAccent,
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
